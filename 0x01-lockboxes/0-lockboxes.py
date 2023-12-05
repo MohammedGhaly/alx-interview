c21ac7175a3f4b21816943051db6124f5008743c
MohammedGhaly <medooghaly7.com>
2023-12-05 12:36:58 +0000

    lockboxes task solved

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''Lockboxes problems'''
+
+
+def canUnlockAll(boxes: list):
+    '''kicks off recursion'''
+    if len(boxes) < 2:
+        return True
+    boxes_map = {i: i == 0 for i in range(len(boxes))}
+    recursive(boxes, boxes_map, keys=boxes[0].copy())
+    return all(value for value in boxes_map.values())
+
+
+def recursive(boxes, boxes_map: dict, keys):
+    '''recursive function to unlock the box'''
+    for key in keys:
+        if not (boxes_map[key] or key >= len(boxes)):
+            boxes_map[key] = True
+            recursive(boxes, boxes_map, boxes[key])
