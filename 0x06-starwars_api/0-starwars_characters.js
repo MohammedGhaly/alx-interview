143765685e4a7d79885ebc6eeb656528d5158f24
MohammedGhaly <medooghaly7.com>
2023-12-08 23:14:00 +0000

    star_wars_api task solved

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+const request = require("request");
+const API_URL = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
+
+// a function to return a list of promises for each character fetched from the api
+function getCharacterNames(characters) {
+  names = characters.map((character) => {
+    return new Promise((resolve, reject) => {
+      request(character, (error, _, body) => {
+        if (error) reject("something went wrong");
+        else resolve(JSON.parse(body).name);
+      });
+    });
+  });
+  return names;
+}
+
+request(API_URL, (error, _, body) => {
+  if (error) console.log(`error: ${error}`);
+  else {
+    const targetFilm = JSON.parse(body);
+    namesPromises = getCharacterNames(targetFilm.characters);
+    Promise.all(namesPromises).then((result) => {
+      result.forEach((element) => {
+        console.log(element);
+      });
+    });
+  }
+});
