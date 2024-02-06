cec508cd7c8435403994938c46af6875401cef83
MohammedGhaly <medooghaly7@gmail.com>
2024-02-06 03:36:05 +0200

    island perimeter problem solved

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""island perimeter problem"""
+
+
+def island_perimeter(grid):
+    '''
+    a function that calculates the perimeter of an island
+    where 1 and 0 represent land and water respectively
+    '''
+    perimeter = 0
+
+    for row, row_value in enumerate(grid):
+        for col, value in enumerate(row_value):
+
+            if value:
+                top = (grid[row-1][col] == 0) if row > 0 else 0
+                bottom = grid[row+1][col] == 0 if row < len(grid)-1 else 0
+                left = grid[row][col-1] == 0 if col > 0 else 0
+                right = grid[row][col +
+                                  1] == 0 if col < len(row_value)-1 else 0
+                perimeter += sum([top, bottom, right, left])
+    return perimeter
